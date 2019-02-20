5c67cf70222e644c56a0bc59ab6944ebe7d38fee
amandaduque <amanda.duque@me.com>
2019-02-20 18:18:08 -0500

    lab 2 update

--- main.js
+++ main.js
@@ -6,31 +6,32 @@ let playGame = prompt('Would you like to play with the Almighty Grant?');
 
 
 
-function points(){
-    return Math.floor((Math.random()*2)+1);
+function points() {
+    return Math.floor((Math.random() * 5) + 1);
 }
 
-if (playGame === "yes"){
+if (playGame === "yes") {
     let username = prompt('What is your name?');
-    while (userPoints > 0 && playerWins < 3){
-    userPoints -= points();
-    grantPoints -= points();  
-    console.log(`${username} has ${userPoints}`); 
-    console.log(`Grant has ${grantPoints}`); 
-    if (grantPoints <= 0){
-        playerWins += 1;
-        grantPoints = 10;
+    while (userPoints > 0 && playerWins < 3) {
+        let attack = prompt('Do you want to attack or quit?');
+        if (attack === 'quit') {
+            console.log('you quit')
+            break;
+        } 
+        userPoints -= points();
+        grantPoints -= points();
+        console.log(`${username} has ${userPoints}`);
+        console.log(`Grant has ${grantPoints}`);
+        if (grantPoints <= 0) {
+            playerWins += 1;
+            grantPoints = 10;
+        }
+        if (playerWins === 3) {
+            console.log(`${username} won!`);
+            break;
+        } else if (userPoints <= 0) {
+            console.log(`${username} lost.`);
+            break;
+        }
     }
-    if (playerWins === 3) {
-        console.log(`${username} won!`);
-        break;
-    } else if (userPoints <= 0){
-        console.log(`${username} lost.`);
-        break;
-    }
-    }
-
-} else if (game === "no" || game !== ""){
-    console.log ("Come back soon!");
 }
-
